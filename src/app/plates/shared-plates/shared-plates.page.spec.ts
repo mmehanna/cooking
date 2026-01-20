889f939f07cc9e9f2965b9c3de562920decb4de3
Moustapha <moustaphamehanna@gmail.com>
2026-01-20 15:00:05 -0500

    Corrected the visualisation of the plates when we choose a plate. Corrected the visualisation of the breakfast, lunch and dinner plates.

--- src/app/plates/shared-plates/shared-plates.page.spec.ts
+++ src/app/plates/shared-plates/shared-plates.page.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SharedPlatesPage } from './shared-plates.page';
+import {async} from "rxjs";
 
 describe('SharedPlatesPage', () => {
   let component: SharedPlatesPage;
   let fixture: ComponentFixture<SharedPlatesPage>;
 
+  // @ts-ignore
   beforeEach(async(() => {
     fixture = TestBed.createComponent(SharedPlatesPage);
     component = fixture.componentInstance;
